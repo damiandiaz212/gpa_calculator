21645c2decb22074afb81bda092f8ad74ef13d3f
damiandiaz212 <n00821283@students.ncc.edu>
2018-05-22 22:54:05 -0400

    Delete readme.sh

--- gpa_calculator/readme.sh
+++
@@ -1,3 +0,0 @@
-Calculate GPA, program does not use Letter grades, grades must be translated as per university
-template, ex at Queens College [B+ = 3.33] whilst at NCC [B+ = 3.5]. Weight is the credits worth 
-not credits earned.
